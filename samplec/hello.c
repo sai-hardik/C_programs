2c1f6e5de242ff415bdbec7d16f9352cd7d278bb
Sai Hardik <74413544+sai-hardik@users.noreply.github.com>
2021-01-08 20:56:02 +0530

    Update hello.c

--- samplec/hello.c
+++ samplec/hello.c
@@ -1,6 +1,6 @@
-#include <stdio.h>              // includes information about standard library
+#include <stdio.h>              // includes information about standard library//
 
-main()                          // defining a function named main that recieves no argument values
+main()                          // defining a function named main that recieves no argument values//
 {
-  	printf("Hello, World"); // the main calls library function printf to print the statement
+  	printf("Hello, World"); // the main calls library function printf to print the statement//
 }
